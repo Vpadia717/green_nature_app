d0cff46e7a5d425e2296100dd33ad47d4f28a00b
Vivek Padia <vivekpadia2706@gmail.com>
2023-02-05 19:32:03 +0530

    Initial commit

--- lib/src/utils/theme/widget_themes/text_theme.dart
+++ lib/src/utils/theme/widget_themes/text_theme.dart
@@ -4,21 +4,25 @@ import 'package:google_fonts/google_fonts.dart';
 class TTextTheme {
   // light theme Context
   static TextTheme lightTextTheme = TextTheme(
+    // ignore: deprecated_member_use
     headline3: GoogleFonts.carroisGothic(
       fontWeight: FontWeight.w200,
       color: const Color(0xFF303030),
       fontSize: 24,
     ),
+    // ignore: deprecated_member_use
     headline6: GoogleFonts.electrolize(
       fontWeight: FontWeight.bold,
       color: const Color(0xFF404040),
       fontSize: 18,
     ),
+    // ignore: deprecated_member_use
     subtitle1: GoogleFonts.poppins(
       fontWeight: FontWeight.w500,
       color: const Color(0xFF606060),
       fontSize: 18,
     ),
+    // ignore: deprecated_member_use
     bodyText1: GoogleFonts.poppins(
       color: const Color(0xFF808080),
       fontSize: 16,
@@ -27,21 +31,25 @@ class TTextTheme {
 
   // dark theme context
   static TextTheme darkTextTheme = TextTheme(
+    // ignore: deprecated_member_use
     headline3: GoogleFonts.carroisGothic(
       fontWeight: FontWeight.w200,
       color: const Color(0xFFEAEAEA),
       fontSize: 24,
     ),
+    // ignore: deprecated_member_use
     headline6: GoogleFonts.electrolize(
       fontWeight: FontWeight.bold,
       color: const Color(0xFFDADADA),
       fontSize: 18,
     ),
+    // ignore: deprecated_member_use
     subtitle1: GoogleFonts.poppins(
       fontWeight: FontWeight.w500,
       color: const Color(0xFFCACACA),
       fontSize: 20,
     ),
+    // ignore: deprecated_member_use
     bodyText1: GoogleFonts.poppins(
       color: const Color(0xFFBABABA),
       fontSize: 16,
